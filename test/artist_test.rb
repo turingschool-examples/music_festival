e876ac209c8ffc79540d7dc354ce95bc8c0c43a2
Brian Zanti <brianzanti@gmail.com>
2018-07-16 08:31:21 -0600

    initial commit

---
+++ test/artist_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '.lib/artist'
+
+class ArtistTest < Minitest::Test
+  def test_it_exists
+    artist = Artist.new("Punch Brothers", "10000")
+    assert_instance_of Artist, artist
+  end
+
+  def test_it_has_attributes
+    artist = Artist.new("Punch Brothers", "10000")
+    assert_equal "Punch Brothers", artist.name
+    assert_equal 10000, artist.salary
+  end
+end
